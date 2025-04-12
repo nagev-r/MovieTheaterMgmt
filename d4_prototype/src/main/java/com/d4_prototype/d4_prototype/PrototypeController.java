c1075a1287db00273ee881ee35e569cb2c25b15d
nagev <nagevallon.r@gmail.com>
2025-04-12 00:25:07 -0400

    Added some basic endpoints, html, and css (part 2)

--- d4_prototype/src/main/java/com/d4_prototype/d4_prototype/PrototypeController.java
+++ d4_prototype/src/main/java/com/d4_prototype/d4_prototype/PrototypeController.java
@@ -1,12 +1,25 @@
 package com.d4_prototype.d4_prototype;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class PrototypeController {
 
-    @RequestMapping("/")
-    public String getHello(){
-        return "Movie Theater Management System!!";
+    @GetMapping("/")
+    public String getHomepage(Model model) {
+        return "index"; // Renders templates/index.html
+    }
+    @GetMapping("/ticket_selection")  //Will update to "/movies/{id}"
+    public String getTicketSelection (Model model) {
+        return "ticketsPage";
+    }
+    @GetMapping("/seat_selection") //Will update to "/movies/{id}/seats"
+    public String getSeatSelection(Model model) {
+        return "seatingPage";
+    }
+    @GetMapping("/checkout")
+    public String getCheckout (Model model) {
+        return "checkoutPage";
     }
 }
