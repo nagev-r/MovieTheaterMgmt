fff010629e640e2f5019466b3c5a21e63611a8cb
nagev <nagevallon.r@gmail.com>
2025-04-12 00:16:37 -0400

    Added some basic endpoints, html, and css

--- d4_prototype/src/main/java/com/d4_prototype/d4_prototype/D4PrototypeApplication.java
+++ d4_prototype/src/main/java/com/d4_prototype/d4_prototype/D4PrototypeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class D4PrototypeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(D4PrototypeApplication.class, args);
 	}
-
 }
